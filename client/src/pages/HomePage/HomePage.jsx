2d8c60c137d6c882257b5103e024a4b7a5782031
Harsh Verma <vharshverma18@gmail.com>
2021-12-24 01:03:54 +0530

    background-home page

---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "../../components/Navbar/Navbar.jsx"
+import './HomePage.css'
+
+function Home(){ 
+    
+        return(
+            <div>
+        <Navbar/>
+        <div class="bg"></div>
+        </div>
+    )
+    
+    
+}
+
+export default Home
