a4565d29d4a98173508f0b07d65a7061707877eb
kanchi12P <kanchipardhi@gmail.com>
2021-12-24 14:05:04 +0530

    added speakers page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar/Navbar";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import HomePage from "./pages/HomePage/HomePage";
 import SpeakersPage from "./pages/SpeakersPage/SpeakersPage";
+import Speaker1 from "./pages/SpeakersPage/Speaker1";
+import Speaker2 from "./pages/SpeakersPage/Speaker2";
+import Speaker3 from "./pages/SpeakersPage/Speaker3";
+import Speaker4 from "./pages/SpeakersPage/Speaker4";
 import TalksPage from "./pages/TalksPage/TalksPage";
 import TeamPage from "./pages/TeamPage/TeamPage";
 
@@ -16,6 +20,11 @@ function App() {
                 <Route path='/speakers' element={<SpeakersPage />} />
                 <Route path='/talks' element={<TalksPage />} />
                 <Route path='/team' element={<TeamPage />} />
+                
+               <Route  path="/speaker1" element={<Speaker1 />} />
+               <Route  path="/speaker2" element={<Speaker2 />} />
+               <Route  path="/speaker3" element={<Speaker3 />} />
+               <Route  path="/speaker4" element={<Speaker4 />} />
             </Routes>
         </>
     );
