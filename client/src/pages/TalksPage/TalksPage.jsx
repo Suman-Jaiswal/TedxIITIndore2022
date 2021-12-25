b290fa2028ff79d042803024e582853085a5e8f5
Mir-Razee <miracle_razee@LAPTOP-BJBBBHGF.localdomain>
2021-12-26 00:43:18 +0530

    Added Talks Card

--- client/src/pages/TalksPage/TalksPage.jsx
+++ client/src/pages/TalksPage/TalksPage.jsx
@@ -1,57 +1,32 @@
 import React from 'react'
-import { Grid,Card} from "@mui/material";
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { NavLink, Link } from "react-router-dom";
-import "./TalksPage.css"
+import { Grid } from "@mui/material";
+import './TalksPage.css'
+import TalksCard from './../../components/TalksCard/TalksCard';
+
 export default function TalksPage() {
-    return (
-        // <div>
-        //     Talks Page
-        //     <iframe width="956" height="538" src="https://www.youtube.com/embed/sCMulrwmN5w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        // </div>
-    <Grid container spacing={4}>
-    <Grid item xs={0} md={0.5}>
-    </Grid>
-    <Grid item xs={12} md={5.5}>
-      
-    <Card style={{padding:'1em',margin:'0.75em'}}>
-      <CardActionArea>
-    <div className='video-responsive'>
-          <iframe width="853" height="480" src="https://www.youtube.com/embed/sCMulrwmN5w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </div>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </Grid>
-    <Grid item xs={12} md={5.5}>
-    <Card style={{padding:'2em',margin:'1em'}}>
-      <CardActionArea>
-        <iframe src="https://www.youtube.com/embed/sCMulrwmN5w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </Grid>
-    <Grid item xs={0} md={0.5}>
-    </Grid>
-</Grid>
-    )
+  return (
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={12} className='header'>
+          <h1 style={{ textAlign: "center", color: "red", margin: "1.25em", fontFamily: "typeface" }}>TALKS</h1>
+        </Grid>
+      </Grid>
+      <Grid container spacing={4}>
+        <Grid item xs={0} md={1}>
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <TalksCard embedId="vIkRT1xF8NY" title="Ms.Neeti Lekha" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!" />
+          <TalksCard embedId="wG1Q1ouemVA" title="Subscribe to TechnoBlade" content="Just do it" />
+          
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <TalksCard embedId="sCMulrwmN5w" title="Dr.Divyanshu Verma" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!" />
+          <TalksCard embedId="lYdEY6Wit_I" title="Yup" content='Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam ratione explicabo officia quia quam, vel aut assumenda soluta iure laboriosam illum officiis, laudantium optio, repellendus fugit? Placeat, distinctio ipsam!' />
+        </Grid>
+        <Grid item xs={0} md={1}>
+        </Grid>
+      </Grid>
+    </>
+
+  )
 }
