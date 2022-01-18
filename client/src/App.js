31e6a602cb98526e4478d1c39bf8ad9a8a34854e
Suman <sumanj631@gmai.com>
2022-01-18 18:37:01 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,9 @@ import { useEffect } from "react";
 function App() {
 
     useEffect(() => {
-        Aos.init()
+        Aos.init({
+            once: true
+        })
      }, [])
 
     return (
