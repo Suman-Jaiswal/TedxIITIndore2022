9932c117e1ecb65f8084eb44e1042bb299b8261e
kanchi12P <kanchipardhi@gmail.com>
2022-01-28 22:27:01 +0530

    transformed images

--- client/src/data/gallery.js
+++ client/src/data/gallery.js
@@ -1,82 +1,82 @@
 export const gallery = [
+   
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273691/images/gallery/9K6A0126.jpg" ,
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273719/images/gallery/9K6A0363.jpg",
         width: 4,
         height: 3
     },
+
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273689/images/gallery/9K6A0300.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_800,c_fill/v1643273720/images/gallery/9K6A0362.jpg",
         width: 1,
         height: 1
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273687/images/gallery/9K6A0219.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_1500,h_900,c_fill/v1643273691/images/gallery/9K6A0258.jpg",
+        width: 5,
+        height: 3
+    },
+    {
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273685/images/gallery/9K6A0284.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273685/images/gallery/9K6A0284.jpg",
-        width: 4,
-        height: 3
-    },
-    {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273663/images/gallery/9K6A0320.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_800,c_fill/v1643273663/images/gallery/9K6A0320.jpg",
         width: 1,
         height: 1
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273662/images/gallery/9K6A0344.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273662/images/gallery/9K6A0344.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273662/images/gallery/9K6A0163.jpg",
-        width: 3,
-        height: 3
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_800,c_fill/v1643273662/images/gallery/9K6A0163.jpg",
+        width: 1,
+        height: 1
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273660/images/gallery/9K6A0063.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273660/images/gallery/9K6A0063.jpg",
         width: 4,
         height: 3
     },
 
 
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273655/images/gallery/9K6A0254.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273655/images/gallery/9K6A0254.jpg",
         width: 4,
         height: 3
     },
    
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273688/images/gallery/9K6A0058.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_800,c_fill/v1643273688/images/gallery/9K6A0106.jpg",
         width: 1,
         height: 1
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273643/images/gallery/9K6A0228.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273643/images/gallery/9K6A0228.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273689/images/gallery/9K6A0300.jpg",
-        width: 3,
-        height: 3
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_800,c_fill/v1643388759/images/gallery/9K6A0056.jpg",
+        width: 1,
+        height: 1
     },
     
-
+ {
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_800,c_fill/v1643273691/images/gallery/9K6A0126.jpg" ,
+        width: 1,
+        height: 1
+    },
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273719/images/gallery/9K6A0363.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273689/images/gallery/9K6A0337.jpg",
         width: 4,
         height: 3
     },
-
     {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273720/images/gallery/9K6A0362.jpg",
-        width: 4,
-        height: 3
-    },
-    {
-        src: "https://res.cloudinary.com/tedxiiti/image/upload/v1643273691/images/gallery/9K6A0258.jpg",
+        src: "https://res.cloudinary.com/tedxiiti/image/upload/w_800,h_600,c_fill/v1643273687/images/gallery/9K6A0219.jpg",
         width: 4,
         height: 3
     }
