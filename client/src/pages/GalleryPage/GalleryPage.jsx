9cdf745e1e58afea4d57f4a3c7b6cdddfa9667ed
Suman <sumanj631@gmai.com>
2022-01-29 12:40:29 +0530

    .

--- client/src/pages/GalleryPage/GalleryPage.jsx
+++ client/src/pages/GalleryPage/GalleryPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useState, useEffect , useCallback} from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import Data from '../../data/Data'
-import Maintenance from '../../components/Maintenance/Maintenance'
+import Gallery  from 'react-photo-gallery'
 import Carousel, { Modal, ModalGateway } from "react-images";
 import Heading from '../../components/Heading';
 import "./GalleryPage.css"
@@ -11,44 +11,44 @@ export default function GalleryPage() {
     useEffect(() => {
         setGallery(Data().gallery)
     }, [])
-    
+
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
 
     const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
 
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
 
-  return (
-    <div className="gallery">
-        <Heading  heading={'GALLERY'} /> 
-        <div className="photos">
-      <Gallery photos={gallery} onClick={openLightbox}  />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={gallery.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
-      </div>
-    </div>
-  );
+    return (
+        <div className="gallery">
+            <Heading heading={'GALLERY'} />
+            <div className="photos">
+                <Gallery photos={gallery} onClick={openLightbox} />
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                        <Modal onClose={closeLightbox}>
+                            <Carousel
+                                currentIndex={currentImage}
+                                views={gallery.map(x => ({
+                                    ...x,
+                                    srcset: x.srcSet,
+                                    caption: x.title
+                                }))}
+                            />
+                        </Modal>
+                    ) : null}
+                </ModalGateway>
+            </div>
+        </div>
+    );
 }
