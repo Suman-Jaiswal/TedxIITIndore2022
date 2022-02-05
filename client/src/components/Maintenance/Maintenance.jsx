a446683db8aa29c844a83de2f0ca03cf3308f945
Suman <sumanj631@gmai.com>
2022-02-05 08:59:58 +0530

    .main

--- client/src/components/Maintenance/Maintenance.jsx
+++ client/src/components/Maintenance/Maintenance.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function Maintenance() {
+export default function Maintenance({ heading }) {
     return (
-        <section className="container-maint">
+        <section className="container-maint bg-light">
             <div className="content">
-                <div className="message">
-                    <h1>Coming Soon...!</h1>
+                <div className="message text-dark">
+                    <h1>{heading}</h1>
+                    <img src="maintenance.webp" height={300} width={300} alt="" />
                     {/* <p>Stay tuned 👌</p> */}
                 </div>
             </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Maintenance from './components/Maintenance/Maintenance';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            {
+                process.env.REACT_APP_MODE === 'maintenance' ? <Maintenance heading={'Under Maintenance'} /> : <App />
+            }
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
