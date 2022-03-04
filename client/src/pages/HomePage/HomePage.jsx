d2242c0a3a7326906e6af80c0e7dff3edeb95cb1
Suman <sumanj631@gmai.com>
2022-03-04 21:11:21 +0530

    .

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -124,13 +124,13 @@ function Home() {
             <section className="location py-5 bg-light" data-aos="fade-up" >
                 <div className="container text-dark">
                     <div className="row my-3">
-                        <div className="h3 col-7 text-center">
+                        <div className="h3 col-12 col-md-7 text-center">
                             <div className="text-danger mb-2">VENUE</div>
                             <div className="lead">
                                 In the campus of IIT Indore, Khandwa road, Simrol
                             </div>
                         </div>
-                        <div className="col-5 bg-danger p-2">
+                        <div className="col-12 col-md-5 p-2">
                             <iframe title="map" className="w-100 m-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44173.68003234819!2d75.89840349008908!3d22.530456862910512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962efcccbce7145%3A0x784e8cb69818596b!2sIndian%20Institute%20Of%20Technology%E2%80%93Indore%20(IIT%E2%80%93Indore)!5e0!3m2!1sen!2sin!4v1646406923144!5m2!1sen!2sin" height={300} allowFullScreen loading="lazy" />
                         </div>
                     </div>
